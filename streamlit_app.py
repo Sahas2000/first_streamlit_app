19caa8bbd42ac833689358bce880eba84a3ccc36
Sahas Mahant <121155953+Sahas2000@users.noreply.github.com>
2022-12-22 11:21:59 +0530

    Update streamlit_app.py
    added beakfast menu header with 4 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,10 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
