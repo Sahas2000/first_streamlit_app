3e019221f4d11c6e25e89f60e74cab8c2b575216
Sahas Mahant <121155953+Sahas2000@users.noreply.github.com>
2022-12-22 11:23:28 +0530

    Update streamlit_app.py
    deleted header and 4 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
-
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
