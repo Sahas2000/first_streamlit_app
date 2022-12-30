1263215dd9bb6e3d8dca703e037d264f0933cea7
Sahas Mahant <121155953+Sahas2000@users.noreply.github.com>
2022-12-30 12:50:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,20 @@ streamlit.write('The user entered',fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
+streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains :")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit=streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ',  add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
