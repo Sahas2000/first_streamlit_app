f429c637943cdff8d7f644d884b17505e023d30d
Sahas Mahant <121155953+Sahas2000@users.noreply.github.com>
2022-12-30 13:42:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,5 +63,5 @@ if streamlit.button('Get fruit load list'):
 add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ',  add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
