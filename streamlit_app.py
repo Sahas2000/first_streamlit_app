1b06118a08869e02b02d3e708b12629d62572300
Sahas Mahant <121155953+Sahas2000@users.noreply.github.com>
2022-12-30 12:56:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,18 +49,7 @@ streamlit.write('Thanks for adding ',  add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 #New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-    
-    
-except URLError as e:
-  streamlit.error()
+
 
 
 
